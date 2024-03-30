1937381237b9243f90abde88d01ce1e3682f50a2
markusflores44 <markuslsflores@gmail.com>
2024-03-29 20:11:24 -0600

    initial MAUI project

---
+++ RestaurantManagementSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantManagementSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
