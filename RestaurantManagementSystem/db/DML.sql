83be810802aef48927a44e7a4fe1535ffefe01f1
lunian16 <lijunxianjunction@gmail.com>
2024-04-06 17:50:49 -0600

    Update DML sql file

--- RestaurantManagementSystem/db/DML.sql
+++ RestaurantManagementSystem/db/DML.sql
@@ -150,3 +150,12 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,001', 'Cheese Burger', 6.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,002', 'Beef Burger', 10.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,003', 'Chicken Burger', 7.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,001', 'Coke', 2.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,002', 'Seven UP', 2.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,003', 'Soda', 2.99);
+
+
