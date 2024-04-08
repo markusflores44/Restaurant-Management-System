351f849536c5c5bd7792f16b4d300ac122b455ad
lunian16 <lijunxianjunction@gmail.com>
2024-04-08 16:39:32 -0600

    MenuPage_update, dml.sql Update

--- RestaurantManagementSystem/db/DML.sql
+++ RestaurantManagementSystem/db/DML.sql
@@ -1,6 +1,14 @@
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,001', 'Cheese Burger', 6.99);
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,002', 'Beef Burger', 10.99);
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,003', 'Chicken Burger', 7.99);
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,001', 'Coke', 2.99);
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,002', 'Seven UP', 2.99);
-INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,003', 'Soda', 2.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (1001, 'Cheese Burger', 6.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (1002, 'Beef Burger', 10.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (1003, 'Chicken Burger', 7.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (2001, 'Coke', 2.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (2002, 'Seven UP', 2.99);
+INSERT INTO `menu_item` (`Item#`, `Item_Name`, `Item_Price`) VALUES (2003, 'Soda', 2.99);
+
+
+INSERT INTO `booth` (`Booth#`, `Booth_Name`) VALUES
+	(1, 'Table 1'),
+	(2, 'Table 2'),
+	(3, 'Table 3'),
+	(4, 'Table 4'),
+	(5, 'Table 5');
