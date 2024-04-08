2f9472bac94160500f56ab1bdcf553b997737edc
markusflores44 <markuslsflores@gmail.com>
2024-04-08 10:42:49 -0600

    Restore DML inserts

---
+++ RestaurantManagementSystem/db/DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,001', 'Cheese Burger', 6.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,002', 'Beef Burger', 10.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('1,003', 'Chicken Burger', 7.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,001', 'Coke', 2.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,002', 'Seven UP', 2.99);
+INSERT INTO menu_item (`Item#`, `Item_Name`, `Item_Price`) VALUES ('2,003', 'Soda', 2.99);
